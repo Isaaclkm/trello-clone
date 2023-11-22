fd45d5ae27ac01f8666ba5007011034613d673dc
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-11-21 21:07:57 -0500

    added more to the landing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@/components/ui/button";
 import {Medal } from "lucide-react";
 import Link from "next/link";
+import { Poppins } from "next/font/google";
 
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
 const headingFont = localFont({
   src: "../../public/font/font.woff2"
+})
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900"
+  ]
 })
 
 const MarketingPage = () => {
@@ -25,8 +41,10 @@ const MarketingPage = () => {
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">Work Forward
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
-        Colaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique- accomplish it all with Taskify. 
+      <div className={cn(
+        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className)}>
+        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique- accomplish it all with Taskify. 
       </div>
       <Button className="mt-6" size="lg" asChild>
         <Link href={"/sign-up"}>
